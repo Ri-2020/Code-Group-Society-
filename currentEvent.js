ec92959a359fe83590c37adab3b032c78637fc52
Rohit Gupta <74013705+Ri-2020@users.noreply.github.com>
2021-09-28 20:34:26 +0530

    29 September 2021

--- currentEvent.js
+++ currentEvent.js
@@ -3,8 +3,9 @@ var currentEvents = [
     {
         title : "Loops",
         content: "Make a simple program you just have to use loop inside a loop (nested loop) in your program.",
-        link : "https://forms.gle/TtLrB85X8VfMnt5F9",
+        link : "https://docs.google.com/document/d/e/2PACX-1vQ3mBmzJvswVUJxXlC3FCAQKw6lFdZAm6f0dv2QhuySdE4JqySHxZqGAQKQzM0X-nkuiZSSDcDj0s3b/pub",
         date: "27 september, 2021",
+        btn: "Answer",
         day: "01"
     },
     {
@@ -12,8 +13,18 @@ var currentEvents = [
       content: "Make a program with two functions the second function will be called inside the first function.",
       link : "https://forms.gle/fj6YUcPGZm9TVjQ39",
       date: "28 september, 2021",
+      btn: "Submit",
       day: "02"
+    },
+    {
+      title : "Recursion",
+      content: "Make a program and use recursion in it, Do not make Factiorial Program.",
+      link : "https://forms.gle/DCGnTkSSzCN6R2Zx5",
+      date: "29 september, 2021",
+      btn: "Submit",
+      day: "03"
     }
+
 ];
 
 function renderTasks(){
@@ -30,7 +41,7 @@ function renderTasks(){
     <p class="content">
     ${currentEvents[i].content}
     </p>
-    <div class="btn">Submit</div>
+    <div class="btn">${currentEvents[i].btn}</div>
   </a>`}
 }
 
