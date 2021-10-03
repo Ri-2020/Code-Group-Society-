a5c3d272050a853b43d3ddf3b456c850e91712fc
Rohit Gupta <74013705+Ri-2020@users.noreply.github.com>
2021-10-03 18:27:29 +0530

    3 october 2021
    New questions are added in cgs's 21 days of code.

--- currentEvent.js
+++ currentEvent.js
@@ -3,9 +3,9 @@ var currentEvents = [
     {
         title : "Loops",
         content: "Make a simple program you just have to use loop inside a loop (nested loop) in your program.",
-        link : "https://docs.google.com/document/d/e/2PACX-1vQ3mBmzJvswVUJxXlC3FCAQKw6lFdZAm6f0dv2QhuySdE4JqySHxZqGAQKQzM0X-nkuiZSSDcDj0s3b/pub",
+        link : "https://forms.gle/tTP7Tixr9Ag9Y7vB8",
         date: "27 september, 2021",
-        btn: "Answer",
+        btn: "Submit",
         day: "01"
     },
     {
@@ -23,7 +23,41 @@ var currentEvents = [
       date: "29 september, 2021",
       btn: "Submit",
       day: "03"
+    },
+    {
+      title : "Analyzer1",
+      content: "Make a program in which user will give an input of an integer. You have to analyze that number and write some outputs such that the number is positive or negative , write its binary form it is prime or not , is it armstrong number. (You can add more if you want) ",
+      link : "https://forms.gle/FoFeg7GUBJw8zm1aA",
+      date: "30 september, 2021",
+      btn: "Submit",
+      day: "04"
+    },
+    {
+      title : "Analyzer2",
+      content: "Make a program in which user will give an input of an string. You have to analyze the length of the string , print the reverse that string , print the ascii codes of all the letter of that string. (Add more if you want). ",
+      link : "https://forms.gle/NmcaPwdv7BCFyRag9",
+      date: "1 October, 2021",
+      btn: "Submit",
+      day: "05"
+    },
+    {
+      title : "Analyzer3",
+      content: "Make a program in which user will give an input of an integer array. you have to print that how many of them are odd , even , positive , negative , zeros , primes, mean of all numbers",
+      link : "https://forms.gle/aqWPc4mUi9r1FMWM8",
+      date: "2 October, 2021",
+      btn: "Submit",
+      day: "06"
+    },
+    {
+      title : "Electricity",
+      content: "Make a program in which you has to create voltage/ Resistance/ Current using the formula (V=IR) means. You  will have 2 inputs out of those three and then find the remian third quantity",
+      link : "https://forms.gle/ApTCuuQ4jV5Xwr3A6",
+      date: "3 October, 2021",
+      btn: "Submit",
+      day: "07"
     }
+
+    
 
 ];
 
@@ -33,6 +67,7 @@ function renderTasks(){
     let itr = 1;
     for( i in currentEvents){
     contt.innerHTML += `<a target="blank" href="${currentEvents[i].link}" class="taskcard">
+    <div class="cardInnerText">
     <div class="cardtitle">
       <h1>${currentEvents[i].day}</h1>
       <h3>${currentEvents[i].title}</h3>
@@ -41,6 +76,7 @@ function renderTasks(){
     <p class="content">
     ${currentEvents[i].content}
     </p>
+    </div>
     <div class="btn">${currentEvents[i].btn}</div>
   </a>`}
 }
